2955fcd216446458de99b3396df41cba0cf14a86
Ramdeniakki <ramdeniakki35@gmail.com>
2024-11-03 11:00:01 +0530

    Added dotenv

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_ADMIN_PASSOWORDKEY = 'tesing)detb:aojd;'
-const JWT_USER_PASSOWORDKEY = "testing_marco_dev_json_epic"
+const JWT_ADMIN_PASSOWORDKEY = process.env.JWT_ADMIN_PASSOWORDKEY
+const JWT_USER_PASSOWORDKEY = process.env.JWT_USER_PASSOWORDKEY
 
 module.exports = {
     JWT_ADMIN_PASSOWORDKEY,JWT_USER_PASSOWORDKEY
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express  = require('express')
 const app = express();
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const {userRouter} = require('./route/user');
 const {courseRouter} = require('./route/course')
@@ -13,7 +13,7 @@ app.use('/api/v1/admin',adminRouter)
 app.use('/api/v1/course',courseRouter)
 
 async function main () {
-await mongoose.connect("mongodb+srv://admin:admin%400701@cluster0.xujxl.mongodb.net/Course-selling-app-database");
+await mongoose.connect(process.env.MONGO_URL);
 app.listen(3000);
 console.log("App is listenig at the Point")
 }
